a7c4a71c57c5f592baf99784f242abf6f0819484
samuraiGH <anonimajzer3@mail.ru>
2025-06-23 14:56:10 +0300

    temp

---
+++ NETGenerator/TypeRefComparer.cs
@@ -0,0 +1,19 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+
+namespace PascalABCCompiler
+{
+	internal class TypeRefComparer : IEqualityComparer<TypeReference>
+	{
+		public bool Equals(TypeReference x, TypeReference y)
+		{
+			return x.FullName == y.FullName;
+		}
+
+		public int GetHashCode(TypeReference obj)
+		{
+			return obj.GetHashCode();
+		}
+	}
+}
